8474500f51f87836808a3ac4667b46813c87e2e9
Joshua Meriyah <jmeriyah@whitebox.co>
2022-10-23 19:29:06 -0400

    Added logging for error

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -3,9 +3,10 @@ import { CustomError } from '../errors/custom-error'
 
 export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
 	if (err instanceof CustomError) {
-		console.log('message', {errors: err.serializeErrors()})
 		return res.status(err.statusCode).send({errors: err.serializeErrors()})
 	}
+
+	console.error(err)
 
 	res.status(400).send({ errors: [{message: err.message}]})
 }
